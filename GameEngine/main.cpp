4b713a5f3097d19b8f3a0e27d93a44a9e649aad5
Ty Roberts <49244692+TheLegion1@users.noreply.github.com>
2021-09-25 16:02:43 -0400

    Initial VS Files Push
    added the VS2019 project files and SDL-2 libraries. No OpenGL yet

---
+++ GameEngine/main.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+
+namespace engine {
+	//engine components go here, them get imported elsewhere to be identified as an engine component
+}
+	 int main() {
+		std::cout << "Hello world";
+		return 0;
+	}
