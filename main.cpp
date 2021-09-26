05e4cbbe7abb9b1cd462fd1327ff5608fab6c79d
DavidBartee <36710065+DavidBartee@users.noreply.github.com>
2021-09-26 13:56:59 -0400

    OpenGL Tutorials 1-8

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+#include"shaderClass.h"
+#include"Texture.h"
+#include"EBO.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"Camera.h"
+
+const unsigned int width = 800, height = 800;
+
+int main() {
+	//Initialize GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Use the core profile for GLFW (modern functions, no outdated ones)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLfloat vertices[] =
+	{ //     COORDINATES     /        COLORS      /   TexCoord  //
+		-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+		-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+		 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+		 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+		 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
+	};
+
+	// Indices for vertices order
+	GLuint indices[] =
+	{
+		0, 1, 2,
+		0, 2, 3,
+		0, 1, 4,
+		1, 2, 4,
+		2, 3, 4,
+		3, 0, 4
+	};
+
+	//Initialize window and error check
+	GLFWwindow *window = glfwCreateWindow(width, height, "OpenGL Demo", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	//Load GLAD, which configures OpenGL
+	gladLoadGL();
+	//Specify the OpenGL viewport
+	glViewport(0, 0, width, height);
+
+	Shader shaderProgram("default.vert", "default.frag");
+
+	VAO VAO1;
+	VAO1.Bind();
+
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	Texture tyler("Textures/tex.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	tyler.texUnit(shaderProgram, "tex0", 0);
+
+	glEnable(GL_DEPTH_TEST);
+
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+	//Main while loop
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		shaderProgram.Activate();
+
+		camera.Inputs(window);
+		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+
+		tyler.Bind();
+		VAO1.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
+		//Take care of GLFW events, including closing the window
+		glfwPollEvents();
+	}
+	//Cleanup when program closes
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	tyler.Delete();
+	shaderProgram.Delete();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
